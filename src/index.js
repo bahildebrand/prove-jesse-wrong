c632fbf04861cf715670db9a9d05c4add1a925d2
Blake Hildebrand <baugusthildebrand@gmail.com>
2019-08-22 23:51:03 -0400

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button} from "reactstrap";
 
 function JesseButton(props) {
     return (
-      <button className="square" onClick={props.onClick}>
+      <Button onClick={props.onClick} color="info">
         Is Jesse wrong?
-      </button>
+      </Button>
     );
   }
 
